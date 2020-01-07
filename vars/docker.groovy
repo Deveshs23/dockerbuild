2f97b20eaa009ca8b2d6286256fc0b9e23f07a3c
ILY3000 <46020982+Deveshs23@users.noreply.github.com>
2020-01-07 14:52:35 +0530

    Create docker.groovy

---
+++ vars/docker.groovy
@@ -0,0 +1,48 @@
+def build(filename, docker_dir, name)
+{
+    stage('Docker Build'){
+        dir("${docker_dir}"){
+            myDockerImage = docker.build("${name}:${currentBuild.number}", "--file ${filename} .")
+                try{
+                    myDockerImage.inside {
+                        echo "Hello Inside the container"
+            
+                    }
+                }
+                catch(err){
+                    echo "Can not login in to container"
+                    throw err
+                    }
+            }
+    }
+}
+def push(name, credential){
+    stage('docker push'){
+        try{
+                echo "Pushing Image into Repo: "
+                withDockerRegistry(credentialsId: "${credential}", url: '')
+                {
+                    def myDockerImage = docker.build("${name}:${currentBuild.number}")
+                    myDockerImage.push("${currentBuild.number}")
+                }    
+            }
+            catch(err){
+                echo "Fail To push Docker Image: "
+                throw err 
+            }
+    }
+}
+def scan(name){
+    stage(scan){
+            try{
+                echo "Scanning Docker Image: "
+                aquaMicroscanner imageName: "${name}:${currentBuild.number}", notCompliesCmd: '', onDisallowed: 'fail', outputFormat: 'html'
+            }
+            catch(err){
+                echo "Docker has high severity vulnerabilities are found"
+                throw err
+            }
+        }
+    }
+}
+
