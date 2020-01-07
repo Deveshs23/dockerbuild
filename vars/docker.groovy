465125a9ffb8021ce5b1f1442878a29143ecf9ff
ILY3000 <46020982+Deveshs23@users.noreply.github.com>
2020-01-07 17:13:44 +0530

    Update docker.groovy

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,25 +1,65 @@
-def build(String tag, String filename, String docker_dir)
-{
-    myDockerImage = docker.build("${tag}:${currentBuild.number}", "--file ${filename} ${docker_dir}")
-        try{
-            myDockerImage.inside {
-                echo "Hello Inside the container"
-    
-            }
+def call(String pFile){
+ 
+    Properties properties = new Properties()
+    File propertiesFile = new File("${pFile}")
+    propertiesFile.withInputStream {
+    properties.load(it)
+    }
+    ansiColor('xterm'){
+//        checkout scm
+        try {
+            build(properties."tag", properties."filename", properties."docker_dir")
         }
-        catch(err){
-            echo "Can not login in to container"
-            throw err
-            }
+        catch (Exeption e){
+            sendNotification("danger", "Docker build Job failed at getting requirements for ${environment}")
+            echo e.toString()
+            throw e
+        }
+        try {
+            scan(properties."tag")
+        }
+        catch (Exeption e){
+            sendNotification("danger", "Dcoker Scan Job failed at getting requirements for ${environment}")
+            echo e.toString()
+            throw e
+        }
+        try {
+            push(properties."tag", properties."credentialID")
+        }
+        catch (Exeption e){
+            sendNotification("danger", "Docker push Job failed at getting requirements for ${environment}")
+            echo e.toString()
+            throw e
+        }
+        
+
+    }
+
 }
 
-def push(name, credential){
-    stage('docker push'){
+def build(String tag, String filename, String docker_dir){
+    stage("Docker Build"){
+        myDockerImage = docker.build("${tag}:${currentBuild.number}", "--file ${filename} ${docker_dir}")
+            try{
+                myDockerImage.inside {
+                    echo "Hello Inside the container"
+        
+                }
+            }
+            catch(err){
+                echo "Can not login in to container"
+                throw err
+                }
+    }
+}
+
+def push(String tag, String credential){
+    stage("Docker Push"){
         try{
                 echo "Pushing Image into Repo: "
                 withDockerRegistry(credentialsId: "${credential}", url: '')
                 {
-                    def myDockerImage = docker.build("${name}:${currentBuild.number}")
+                    def myDockerImage = docker.build("${tag}:${currentBuild.number}")
                     myDockerImage.push("${currentBuild.number}")
                 }    
             }
@@ -29,17 +69,21 @@ def push(name, credential){
             }
     }
 }
-def scan(name){
-    stage(scan){
-            try{
+
+def scan(String tag)
+{
+    stage("Docker scan"){
+        try{
                 echo "Scanning Docker Image: "
-                aquaMicroscanner imageName: "${name}:${currentBuild.number}", notCompliesCmd: '', onDisallowed: 'fail', outputFormat: 'html'
+                aquaMicroscanner imageName: "${tag}:${currentBuild.number}", notCompliesCmd: '', outputFormat: 'html'
             }
             catch(err){
                 echo "Docker has high severity vulnerabilities are found"
                 throw err
             }
-        }
     }
 }
 
+def sendNotification(status, message) {
+    slackSend channel: 'build-status', color: "${status}", message: " ${message}.\n Message:- Please go to ${env.BUILD_URL} for console output"
+}
