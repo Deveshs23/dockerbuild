e585565271e4d364d91f775fd9c0f1cd67e65a18
ILY3000 <46020982+Deveshs23@users.noreply.github.com>
2020-01-07 22:02:26 +0530

    Update dockerTest.groovy

--- vars/dockerTest.groovy
+++ vars/dockerTest.groovy
@@ -52,6 +52,20 @@ def build(String tag, String filename, String docker_dir){
     }
 }
 
+def scan(String tag)
+{
+    stage("Docker scan"){
+        try{
+                echo "Scanning Docker Image: "
+                aquaMicroscanner imageName: "${tag}:${currentBuild.number}", notCompliesCmd: '', outputFormat: 'html'
+            }
+            catch(err){
+                echo "Docker has high severity vulnerabilities are found"
+                throw err
+            }
+    }
+}
+
 def push(String tag, String credential){
     stage("Docker Push"){
         try{
@@ -69,19 +83,6 @@ def push(String tag, String credential){
     }
 }
 
-def scan(String tag)
-{
-    stage("Docker scan"){
-        try{
-                echo "Scanning Docker Image: "
-                aquaMicroscanner imageName: "${tag}:${currentBuild.number}", notCompliesCmd: '', outputFormat: 'html'
-            }
-            catch(err){
-                echo "Docker has high severity vulnerabilities are found"
-                throw err
-            }
-    }
-}
 
 def sendNotification(status, message) {
     slackSend channel: 'build-status', color: "${status}", message: " ${message}.\n Message:- Please go to ${env.BUILD_URL} for console output"
