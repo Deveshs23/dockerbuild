ac057a4cf23f977c2ccda9cd9c70b2559ec752d8
ILY3000 <46020982+Deveshs23@users.noreply.github.com>
2020-01-13 19:08:29 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,12 +1,13 @@
 def readCustumerName(String file){
 
     sh "echo 'Hello World'"
-    //  def path = "${workspace}/${file}"
-    //     sh(script: """
-    //         while IFS= read -r line
-    //         do
-    //             echo "$line"
-    //         done < "${path}"
-    //     """, returnStdout: true))
-    
+ 
+            sh(script: """
+            path='file.txt'
+            while IFS= read -r line
+            do
+                echo $line
+            done < $path
+        """, returnStdout: true))
+
 }
