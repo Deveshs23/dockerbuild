7de69966ec1d8a257439292dc6a43741297a6445
ILY3000 <46020982+Deveshs23@users.noreply.github.com>
2020-01-13 16:38:07 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,5 +1,5 @@
 def readCustumerName(String filePath) {
-    File file = new File(${workspace}/${filePath})
+    File file = new File("${workspace}/${filePath}")
     def line, noOfLines = 0;
     file.withReader { reader ->
         while ((line = reader.readLine()) != null) {
