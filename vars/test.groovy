164cb59dba276b807c2af183ee41911ca8c5dcef
ILY3000 <46020982+Deveshs23@users.noreply.github.com>
2020-01-13 17:11:50 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,5 +1,5 @@
 def readCustumerName(String file){
-     new File("${workspace}/${file}").eachLine {String line ->
+     new File("${workspace}/${file}").reader {String line ->
         while("${line}" != null)
         println("${line}")
         line++
