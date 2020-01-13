a5515add01aaddbd680ca07724c70a42fbd6a881
ILY3000 <46020982+Deveshs23@users.noreply.github.com>
2020-01-13 19:47:02 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,7 +1,7 @@
 def readCustumerName(String file){
 
   def list_of_custumer = ['linux', 'windows'] as String[]
-    list_of_custumer.each {arch ->
-        println("${arch}")
+    list_of_custumer.each {line ->
+        println("${line}")
   }
 }
