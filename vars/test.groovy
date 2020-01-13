da41dafe578447f77f4d180e24cfc40059e6aec5
ILY3000 <46020982+Deveshs23@users.noreply.github.com>
2020-01-13 16:31:27 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,5 +1,5 @@
 def readCustumerName(String file){
     new File("${workspace}/${file}").eachLine { line ->
-        sh "helm upgrade  -f values.yaml ${line} ./ --namespace producer --install"
+        println("${line}")
     }
 }
