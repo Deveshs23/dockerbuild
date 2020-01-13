228e5437af47d68a02f4acf36ce42dd5e6a2b558
ILY3000 <46020982+Deveshs23@users.noreply.github.com>
2020-01-13 19:38:17 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,12 +1,7 @@
 def readCustumerName(String file){
 
-    sh "echo 'Hello World'"
- 
-            sh """
-            path='file.txt'
-            while IFS= read -r line
-            do
-                echo $line
-            done < $path
-        """
+  ['linux', 'windows'].each {arch ->
+    println("${arch}")
+  }
+    
 }
